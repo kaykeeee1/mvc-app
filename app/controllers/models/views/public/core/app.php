fb00f29c56aff090d4cab2a3ced0c5b08fd0ec06
kaykeeee1 <kaykeevangelista0@gmail.com>
2024-09-23 11:07:16 -0300

    refacture.kayke app.php

--- app/controllers/models/views/public/core/app.php
+++ app/controllers/models/views/public/core/app.php
@@ -1,44 +1,62 @@
-<?Php
+<?php
 
-class app{
-    //definir controlador e método padrão
-    protected $controller = 'UserControler';
-
+class App {
+    // Definir controlador e método padrão
+    protected $controller = 'UserController';
     protected $method = 'index';
-
     protected $params = [];
 
-    public function __construct(){
-        //Obtem a URl e a transforma em um array 
+    public function __construct() {
+        // Obtem a URL e a transforma em um array 
+        $url = $this->parse_url();
 
-        $url = $this-> parse_url();
-
-        //verifica se o controlador existe no diretorio de controladores
-
-        if(file_exists('../app/Controllers/'.$url[0].'.php'))
-        {
-            //Remove o primeiro elemento do array (controlador)para continuar o processando
-            unset($user[0]);
-        }
-        //Requerer o arquivo controlador
-        require_once '../app/controllers/'.$this->controller;
-        //Instanciando o controlador
-        $this->controller = new $this->controller;
-        //Verifica se o controlador existe no diretorio de controladores
-
-        //verificar se existe o método (ação) mp controlador a ser chamado
-        if(file_exists('../app/controller/'.$url[0].'php')){
-            //define o controlador a partir da URL
-            $this->controller=$url[0];
-            //remove o primeiro elemento do ARRAY (controlador)para continuar procssando
+        // Verifica se o controlador existe no diretório de controladores
+        if (file_exists('../app/controllers/' . $url[0] . '.php')) {
+            // Remove o primeiro elemento do array (controlador) para continuar o processamento
             unset($url[0]);
         }
-        //requer o arquivo do controlador
-        require_once '../app/controller/'.$this->controller.'.php';
-        //instancia o controlador
+
+        // Requerer o arquivo controlador
+        require_once '../app/controllers/' . $this->controller . '.php';
+
+        // Instanciando o controlador
         $this->controller = new $this->controller;
 
-        //verifica se existe o método (ação) no controlador a ser chamado
+        // Verificar se existe o método (ação) no controlador a ser chamado
+        if (isset($url[0]) && file_exists('../app/controllers/' . $url[0] . '.php')) {
+            // Define o controlador a partir da URL
+            $this->controller = $url[0];
+            // Remove o primeiro elemento do array (controlador) para continuar processando
+            unset($url[0]);
+        }
+
+        // Requer o arquivo do controlador
+        require_once '../app/controllers/' . $this->controller . '.php';
+
+        // Instancia o controlador
+        $this->controller = new $this->controller;
+
+        // Verifica se existe o método (ação) no controlador a ser chamado
+        if (isset($url[1]) && method_exists($this->controller, $url[1])) {
+            // Define o método (ação) a ser chamado
+            $this->method = $url[1];
+            // Remove o segundo elemento do array (método)
+            unset($url[1]);
+        }
+
+        // Define os parâmetros restantes como parâmetros para o método
+        $this->params = $url ? array_values($url) : [];
+
+        // Chama o método do controlador com os seguintes parâmetros
+        call_user_func_array([$this->controller, $this->method], $this->params);
+    }
+
+    // Função para quebrar a url em partes (controlador, método e parâmetro)
+    public function parse_url() {
+        if (isset($_GET['url'])) {
+            // Remove barras extras e sanitiza a URL
+            return explode('/', filter_var(rtrim($_GET['url'], '/'), FILTER_SANITIZE_URL));
+        }
+        return [];
     }
 }
-?>
