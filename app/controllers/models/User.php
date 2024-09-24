69804b277c9e4e76b99b349938c9ef7b2a2ec887
kaykeeee1 <kaykeevangelista0@gmail.com>
2024-09-24 09:24:40 -0300

    refacture. kayke correção no User.php

--- app/controllers/models/User.php
+++ app/controllers/models/User.php
@@ -3,7 +3,7 @@
 
 class user{
     //Simulando a obtenção de dados do usuario
-    public function getUser(){
+    public function getUsers(){
         // Em um caso real, voce teria a conexão com o banco de dados
         return[
             ['id'=>1,'name'=>'joão','email'=>'joão@email.com'], ['id'=>2, 'name'=> 'maria','email'=>'maria@email.com'],
