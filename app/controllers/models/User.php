dc1d2432c8840c130bd7d89c9a947e0a2d265120
kaykeeee1 <kaykeevangelista0@gmail.com>
2024-09-24 08:32:40 -0300

    initial. kayke User.php

---
+++ app/controllers/models/User.php
@@ -0,0 +1,13 @@
+<?php
+//este arquivo sera responsavel por lidar com os dados dos usuarios.
+
+class user{
+    //Simulando a obtenção de dados do usuario
+    public function getUser(){
+        // Em um caso real, voce teria a conexão com o banco de dados
+        return[
+            ['id'=>1,'name'=>'joão','email'=>'joão@email.com'], ['id'=>2, 'name'=> 'maria','email'=>'maria@email.com'],
+        ];
+    }
+}
+?>
