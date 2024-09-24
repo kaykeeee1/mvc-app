3508cc261f61b975642d53e653a2da0c032e8f28
kaykeeee1 <kaykeevangelista0@gmail.com>
2024-09-24 08:55:09 -0300

    initial. kayke index.php

---
+++ app/controllers/models/views/public/index.php
@@ -0,0 +1,11 @@
+<?php
+//Arquivo principal que inicia o aplicativo. ele deve estar na pasta Public
+
+//Incluir os arquivos principais da aplicação
+require_once '../app/core/app.php';
+require_once '../app/core/controller.php';
+
+//inicia a aplicação
+$app =  new App();
+
+?>
